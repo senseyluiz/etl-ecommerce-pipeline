8ec51cf10eb9ea0f9a0bf63caf5fddc0c434724f
Luis Henrique <senseyluiz@gmail.com>
2026-04-30 17:37:50 -0300

    :sparkles: feat(extract_cart_products) Cria f extract_cart_productsunção que extrai dados de venda do carrinho

--- extract.py
+++ extract.py
@@ -17,5 +17,29 @@ def extract_products():
         }
         products.append(product_data)
     return products
+
+def extract_cart_products():
+    SALES_URL = "https://fakestoreapi.com/carts"
+    response = requests.get(SALES_URL)
+    response.raise_for_status()
+    data = response.json()
+    sales = []
+    # Extrair dados necessários do carrinho
+    for sale in data:
+        sale_data = {
+            "cart_id": sale["id"],
+            "user_id": sale["userId"],
+            "date": sale["date"],
+            "products":[
+                {
+                    "product_id": product["productId"],
+                    "quantity": int(product["quantity"])
+                }
+                for product in sale["products"]
+            ]
+        }
+        sales.append(sale_data)
+    return sales
 if __name__ == "__main__":
     products = extract_products()
+    cart_products = extract_cart_products()
